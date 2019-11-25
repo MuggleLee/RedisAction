ddf100a2bbd6df8b94ee5cfc0b7312e442f905fc
MuggleLee <770796059@qq.com>
2019-11-25 14:19:57 +0800

    add

---
+++ src/main/java/com/hao/redisaction/RedisActionApplication.java
@@ -0,0 +1,13 @@
+package com.hao.redisaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisActionApplication.class, args);
+    }
+
+}
